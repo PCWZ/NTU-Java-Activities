c44a3ac9c36a4391885ecf7abfa8b6122f2dd157
PCWZ <parrychuawz@gmail.com>
2023-10-21 23:58:23 +0800

    Added Activity 1 folder & round prints activity

---
+++ Activity1/VariableDataType.java
@@ -0,0 +1,17 @@
+class VariableDataType {
+    public static void main(String[] args) {
+
+        /* 
+            Task 1: Cast "price" as integer and assign it to "round".            
+        */
+        float price = 9.9f;
+        int round = 0;
+
+        // Add code here
+        round = (int) price;
+        
+        System.out.println("round prints " + round); // prints 9
+
+        
+    }
+}
