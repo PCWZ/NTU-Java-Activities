f9b99428b840d9310270681d16f92f3a9942ff22
PCWZ <parrychuawz@gmail.com>
2023-10-22 00:07:03 +0800

    Enum and if else statement task

--- Activity2/ControlFlowAssignment.java
+++ Activity2/ControlFlowAssignment.java
@@ -3,7 +3,7 @@ package Activity2;
 public class ControlFlowAssignment {
 
     public static void main(String[] args) {
-        
+
         //    Task 1. Determine if a number is divisible by 3, 5 or both.
         int numToTest = 995;
         boolean isDivisible;
@@ -38,6 +38,40 @@ public class ControlFlowAssignment {
             isDivisible = false;
             System.out.println(isDivisible);
         }
+
+         /*
+         * Task 2. Given a sample budget, determine what to ride given the following
+         * conditions:
+         * - if budget is greater than 100, ride a taxi
+         * - if budget is between 50 to 100, ride train
+         * - if budget is between 20 to 49, ride bus
+         * - if budget is between 0 to 19, walk
+         * 
+         * Reference: What is enum? https://www.w3schools.com/java/java_enums.asp
+         */
+        float budget = 72.7f;
+        enum TransportMode {
+            TAXI,
+            TRAIN,
+            BUS,
+            WALK
+        }
+        TransportMode mode = TransportMode.WALK;
+
+        if (budget > 100) {
+            mode = TransportMode.TAXI;
+        }
+
+        else if (budget >= 50 && budget <= 100) {
+            mode = TransportMode.TRAIN;
+        }
+
+        else if (budget >= 20 && budget <= 49) {
+            mode = TransportMode.BUS;
+        }
+
+        System.out.println(mode);
+
 
    
     }
